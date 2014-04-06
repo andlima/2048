6930646b3aacf11f2cb7b4d321d294e7abfc0e36
Andre Lima <andre.lima@gmail.com>
2014-04-06 16:54:43 -0300

    Fix score tweet

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -155,13 +155,13 @@ HTMLActuator.prototype.scoreTweetButton = function () {
   var tweet = document.createElement("a");
   tweet.classList.add("twitter-share-button");
   tweet.setAttribute("href", "https://twitter.com/share");
-  tweet.setAttribute("data-via", "gabrielecirulli");
-  tweet.setAttribute("data-url", "http://git.io/2048");
-  tweet.setAttribute("data-counturl", "http://gabrielecirulli.github.io/2048/");
+  tweet.setAttribute("data-via", "andlima");
+  tweet.setAttribute("data-url", "http://git.io/tB73tw");
+  tweet.setAttribute("data-counturl", "http://andlima.github.io/minus2048/");
   tweet.textContent = "Tweet";
 
-  var text = "I scored " + this.score + " points at 2048, a game where you " +
-             "join numbers to score high! #2048game";
+  var text = "I scored " + this.score + " points at -2048, a game where you " +
+             "try to block the grid!";
   tweet.setAttribute("data-text", text);
 
   return tweet;
